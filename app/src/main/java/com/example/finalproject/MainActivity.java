68c41c2bb692b452ff7b7c19411c39f6be88ec48
Sam-Miller <samuelmi@unc.edu>
2021-11-03 10:07:30 -0400

    Created UI and implemented image capturing

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.finalproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+
+    ImageView iv;
+    Button captureBtn;
+
+    Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+
+        iv = findViewById(R.id.iv);
+        captureBtn = findViewById(R.id.capture_btn);
+
     }
+
+    public void click(View v) {
+        int id = v.getId();
+
+        // Capture is clicked
+        if (id == R.id.capture_btn) {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        image = (Bitmap) data.getExtras().get("data");
+        iv.setImageBitmap(image);
+    }
+
 }
