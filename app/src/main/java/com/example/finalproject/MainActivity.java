c39b5eebf91b714c47f4fbcefb1dead5f444d0b1
Sam-Miller <samuelmi@unc.edu>
2021-11-03 10:09:44 -0400

    Added code from class example for labeling images

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,11 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
@@ -47,6 +54,42 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         image = (Bitmap) data.getExtras().get("data");
         iv.setImageBitmap(image);
+    }
+
+    void getImageLabels() throws IOException
+    {
+        //1. ENCODE image.
+        Bitmap bitmap = ((BitmapDrawable)getResources().getDrawable(R.drawable.b1)).getBitmap();
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bout);
+        Image myimage = new Image();
+        myimage.encodeContent(bout.toByteArray());
+
+        //2. PREPARE AnnotateImageRequest
+        AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+        annotateImageRequest.setImage(myimage);
+        Feature f = new Feature();
+        f.setType("LABEL_DETECTION");
+        f.setMaxResults(5);
+        List<Feature> lf = new ArrayList<Feature>();
+        lf.add(f);
+        annotateImageRequest.setFeatures(lf);
+
+        //3.BUILD the Vision
+        HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+        builder.setVisionRequestInitializer(new VisionRequestInitializer(getResources().getString(R.string.api_key)));
+        Vision vision = builder.build();
+
+        //4. CALL Vision.Images.Annotate
+        BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+        List<AnnotateImageRequest> list = new ArrayList<AnnotateImageRequest>();
+        list.add(annotateImageRequest);
+        batchAnnotateImagesRequest.setRequests(list);
+        Vision.Images.Annotate task = vision.images().annotate(batchAnnotateImagesRequest);
+        BatchAnnotateImagesResponse response = task.execute();
+        Log.v("MYTAG", response.toPrettyString());
     }
 
 }
