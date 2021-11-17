181ba7849424e4cd033ec865dfdd029631ae5060
Sam <samuelmi@live.unc.edu>
2021-11-16 21:21:13 -0500

    Added mp3 files of child saying letters

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -2,13 +2,31 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class GameActivity extends AppCompatActivity {
+    Context context;
+
+    boolean animate = false;
+    ImageButton btnToAnimate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        context = getApplicationContext();
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.music); // TODO: call stop() method when finished or regret your life choices
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
+
+    public void resetGame() {
+        animate = false;
+
+    }
+
 }
--- app/src/main/java/com/example/finalproject/MenuActivity.java
+++ app/src/main/java/com/example/finalproject/MenuActivity.java
@@ -3,6 +3,7 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +23,8 @@ public class MenuActivity extends AppCompatActivity {
         MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.boing);
         mediaPlayer.start();
         if (v.getId() == R.id.start_btn) {
-
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
         }
     }
 
