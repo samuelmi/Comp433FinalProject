c7d2508e0ad3f2d4a981e3f49ed1ad09def85ee6
Sam <samuelmi@live.unc.edu>
2021-11-16 20:09:59 -0500

    Added sound effects to main menu

--- app/src/main/java/com/example/finalproject/MenuActivity.java
+++ app/src/main/java/com/example/finalproject/MenuActivity.java
@@ -1,13 +1,29 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        context = getApplicationContext();
     }
+
+    public void onClick(View v) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.boing);
+        mediaPlayer.start();
+        if (v.getId() == R.id.start_btn) {
+
+        }
+    }
+
 }
