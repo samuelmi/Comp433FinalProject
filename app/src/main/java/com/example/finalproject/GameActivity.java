a56d210dcccb1ae4d99fae652a12e452a070b7da
Sam <samuelmi@live.unc.edu>
2021-11-21 20:10:20 -0500

    Got timer and animations working

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -1,32 +1,67 @@
 package com.example.finalproject;
 
+import static com.example.finalproject.R.drawable.right;
+import static com.example.finalproject.R.drawable.wrong;
+
+import static java.lang.Thread.sleep;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 public class GameActivity extends AppCompatActivity {
     Context context;
 
+    TextView timer;
+    ImageView iv;
+
+    Bitmap image;
+
     boolean animate;
     String letterToAnimate;
     ArrayList<String> remainingLetters = new ArrayList<>();
+
+    long timeStart;
 
     MediaPlayer m;
 
@@ -37,6 +72,7 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         context = getApplicationContext();
+        timer = findViewById(R.id.timer);
         startGame();
     }
 
@@ -101,6 +137,7 @@ public class GameActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
+                                timer.setText(""+(System.currentTimeMillis() - timeStart) / 1000);
                                 Animation anim = new ScaleAnimation(
                                         1f, 1f, // Start and end values for the X axis scaling
                                         0, 1, // Start and end values for the Y axis scaling
@@ -115,6 +152,7 @@ public class GameActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
+                                timer.setText(""+(System.currentTimeMillis() - timeStart) / 1000);
                                 Animation anim = new ScaleAnimation(
                                         1f, 1f, // Start and end values for the X axis scaling
                                         1, 0, // Start and end values for the Y axis scaling
@@ -140,6 +178,7 @@ public class GameActivity extends AppCompatActivity {
         Collections.shuffle(remainingLetters);
         letterToAnimate = remainingLetters.remove(0);
         Log.v("MyTag", "Letter to animate: "+letterToAnimate);
+        timeStart = System.currentTimeMillis(); // Resets timer
         animateLetter();
     }
 
@@ -147,6 +186,121 @@ public class GameActivity extends AppCompatActivity {
         animate = false;
         remainingLetters = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
         letterToAnimate = "";
+    }
+
+
+    public void click(View v) {
+        int id = v.getId();
+
+        iv = findViewById(id);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        image = (Bitmap) data.getExtras().get("data");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getImageLabels();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    void getImageLabels() throws IOException
+    {
+        //1. ENCODE image.
+        Bitmap bitmap = image;
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bout);
+        Image myimage = new Image();
+        myimage.encodeContent(bout.toByteArray());
+
+        //2. PREPARE AnnotateImageRequest
+        AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+        annotateImageRequest.setImage(myimage);
+        Feature f = new Feature();
+        f.setType("LABEL_DETECTION");
+        f.setMaxResults(3);
+        List<Feature> lf = new ArrayList<Feature>();
+        lf.add(f);
+        annotateImageRequest.setFeatures(lf);
+
+        //3.BUILD the Vision
+        HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+        builder.setVisionRequestInitializer(new VisionRequestInitializer(getResources().getString(R.string.api_key)));
+        Vision vision = builder.build();
+
+        //4. CALL Vision.Images.Annotate
+        BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+        List<AnnotateImageRequest> list = new ArrayList<AnnotateImageRequest>();
+        list.add(annotateImageRequest);
+        batchAnnotateImagesRequest.setRequests(list);
+        Vision.Images.Annotate task = vision.images().annotate(batchAnnotateImagesRequest);
+        BatchAnnotateImagesResponse response = task.execute();
+
+        EntityAnnotation[] annotations = response.getResponses().get(0).getLabelAnnotations().toArray(new EntityAnnotation[0]);
+        boolean correct = false;
+        for (int i = 0; i < annotations.length; i ++) {
+            EntityAnnotation a = annotations[i];
+            // If the photo is taken of something that starts with the right letter, mark correct
+            if (a.getDescription().substring(0,1).toLowerCase().equals(letterToAnimate)) {
+                correct = true;
+                break;
+            }
+        }
+
+        getNextLetter(); // Gets the next letter
+
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        boolean finalCorrect = correct;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (finalCorrect) {
+                    iv.setImageDrawable(getResources().getDrawable(right));
+                }
+                else {
+                    iv.setImageDrawable(getResources().getDrawable(wrong));
+                }
+                Animation anim = new ScaleAnimation(
+                        1f, 1f, // Start and end values for the X axis scaling
+                        0, 1, // Start and end values for the Y axis scaling
+                        Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
+                        Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
+                anim.setFillAfter(true); // Needed to keep the result of the animation
+                anim.setDuration(1000);
+                iv.startAnimation(anim);
+            }
+        });
+
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                iv.setImageBitmap(image); // Sets image bitmap
+            }
+        });
     }
 
 }
